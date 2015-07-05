c31dea7bb9dc104c23cf6960f61d56af86c8dea6
viktormalyi <v.stratus@gmail.com>
2015-07-05 16:00:30 +0200

    Update package description and version

--- setup.py
+++ setup.py
@@ -12,15 +12,14 @@ test_requirements = [
 
 setup(
     name='adb_android',
-    version='0.1.0',
+    version='0.2.0',
     description="Enables android adb in your python script",
-    long_description='This package can be used by everyone who implements some \
-    android-related stuff on Python and at the same time has to interact with \
-    android adb. It makes interaction with android adb easier because of proper \
-    error handling and some useful features.',
+    long_description='This python package is a wrapper for standard android adb\
+    implementation. It allows you to execute android adb commands in your \
+    python script.',
     author='Viktor Malyi',
     author_email='v.stratus@gmail.com',
-    url='https://github.com/vmalyi/adb',
+    url='https://github.com/vmalyi/adb_android',
     packages=[
         'adb_android',
     ],
