f91ada6ccc5371a770e8155fa06e6d82cf7bc4b4
Viktor Malyi <viktor.malyi@garmin.com>
2016-01-29 13:18:34 +0100

    Refactor adb.install()
    Change-Id: I25a90ba4c51c0c1ff5c32c9a13a2799cdb8a3fdd

--- adb_android/adb_android.py
+++ adb_android/adb_android.py
@@ -1,57 +1,82 @@
-import os
 import tempfile
-import var as v
 from subprocess import check_output, CalledProcessError
 
+import var as v
+
+
 def push(src, dest):
-    """Pushes files and folders to device."""
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PUSH, src, dest ]
+    """
+    Push object from host to target
+    :param src: string path to source object on host
+    :param dest: string destination path on target
+    :return: result of exec_command() execution
+    """
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PUSH, src, dest]
     return exec_command(adb_full_cmd)
+
 
 def pull(src, dest):
-    """Pulls files and folders to device."""
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, src, dest ]
+    """
+    Pull object from target to host
+    :param src: string path of object on target
+    :param dest: string destination path on host
+    :return: result of exec_command() execution
+    """
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, src, dest]
     return exec_command(adb_full_cmd)
+
 
 def devices(opt_l=''):
-    """Provides list of available devices"""
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_DEVICES, opt_l ]
-    return exec_command(adb_full_cmd)
-
-def shell(subcommand):
-    """Executes subcommand in adb shell
-
-    accepts string as "subcommand" argument
-    example: "adb shell cat filename.txt"
-
     """
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_SHELL, subcommand ]
-    return exec_command(adb_full_cmd)
-
-def install(apk, opt_r='', opt_s='', opt_l='', opt_d='', opt_t=''):
-    """Installs apk on device.
-
-    options:
-        -r: replace existing application
-        -s: install application on sdcard
-        -l: forward lock application
-        -d: reinstall existing apk
-        -t: allow test packages
-
+    Get list of all available devices including emulators
+    :param opt_l: provides additional info on device
+    :return: result of exec_command() execution
     """
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_INSTALL, opt_r, opt_s, \
-    opt_l, opt_d, opt_t, apk ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_DEVICES, opt_l]
     return exec_command(adb_full_cmd)
+
+
+def shell(cmd):
+    """
+    Execute shell command on target
+    :param cmd: string shell command to execute
+    :return: result of exec_command() execution
+    """
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_SHELL, cmd]
+    return exec_command(adb_full_cmd)
+
+
+def _convert_opts(opts):
+    """
+    Convert list with command options to single string value
+    with 'space' delimeter
+    :param opts: list with space-delimeted values
+    :return: string with space-delimeted values
+    """
+    return ' '.join(opts)
+
+
+def install(apk, opts):
+    """
+    Install *.apk on target
+    :param opts: list command options (e.g. ["-r", "-a"])
+    :param apk: string path to apk on host to install
+    :return: result of exec_command() execution
+    """
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_INSTALL, _convert_opts(opts), apk]
+    return exec_command(adb_full_cmd)
+
 
 def uninstall(apk, opt_k=''):
-    """Uninstalls apk from device.
-
-    options:
-        -k: keep the data and cache directories
-
     """
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_UNINSTALL, apk, opt_k ]
+
+    :param apk:
+    :param opt_k: keep the data and cache directories
+    :return:
+    """
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_UNINSTALL, apk, opt_k]
     return exec_command(adb_full_cmd)
+
 
 def getserialno():
     '''Gets serial number for all online devices
@@ -63,8 +88,9 @@ def getserialno():
         String device serial number
 
     '''
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_GETSERIALNO ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_GETSERIALNO]
     return exec_command(adb_full_cmd)
+
 
 def wait_for_device():
     '''Waits until device is online
@@ -75,8 +101,9 @@ def wait_for_device():
     returns:
         0 if command has been executed successfully.
     '''
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_WAITFORDEVICE ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_WAITFORDEVICE]
     return exec_command(adb_full_cmd)
+
 
 def start_server():
     '''Start adb server daemon
@@ -87,8 +114,9 @@ def start_server():
     returns:
         0 if command has been executed successfully.
     '''
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_START_SERVER ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_START_SERVER]
     return exec_command(adb_full_cmd)
+
 
 def kill_server():
     '''Stops adb server daemon
@@ -99,8 +127,9 @@ def kill_server():
     returns:
         0 if command has been executed successfully.
     '''
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_KILL_SERVER ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_KILL_SERVER]
     return exec_command(adb_full_cmd)
+
 
 def get_state():
     '''Gets current state of device connected per adb
@@ -111,8 +140,9 @@ def get_state():
     returns:
         0 if command has been executed successfully.
     '''
-    adb_full_cmd = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_GET_STATE ]
+    adb_full_cmd = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_GET_STATE]
     return exec_command(adb_full_cmd)
+
 
 def exec_command(adb_full_cmd):
     """Executes adb command and handles result code.
@@ -124,13 +154,13 @@ def exec_command(adb_full_cmd):
     if adb_full_cmd is not None:
         try:
             t = tempfile.TemporaryFile()
-            #removes empty list elements if func argument hasn't been used
+            # removes empty list elements if func argument hasn't been used
             final_adb_full_cmd = []
             for e in adb_full_cmd:
                 if e != '':
                     final_adb_full_cmd.append(e)
             print('\n*** executing ' + ' '.join(adb_full_cmd) + ' ' \
-            + 'command')
+                  + 'command')
             output = check_output(final_adb_full_cmd, stderr=t)
             result = 0, output
         except CalledProcessError as e:
--- tests/test_adb_android.py
+++ tests/test_adb_android.py
@@ -1,9 +1,17 @@
-import sys, os, inspect, re, tempfile, unittest
-adb_android = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],'../adb_android')))
+import inspect
+import os
+import re
+import sys
+import tempfile
+import unittest
+
+adb_android = os.path.realpath(
+    os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], '../adb_android')))
 if adb_android not in sys.path:
     sys.path.insert(0, adb_android)
 import adb_android as adb
 import var as v
+from sure import expect
 
 DEST_FOLDER_TARGET = '/data/media/0/'
 NON_EXISTING_DIR = '/non-existing-dir/'
@@ -24,8 +32,10 @@ adb_pull = None
 POSITIVE_EXP_RESULT_WO_OUTPUT = 0, ''
 POSITIVE_EXP_RESULT = 0
 
+
 def setUpModule():
     generate_tmp_file()
+
 
 def generate_tmp_file():
     """Generates temp file and pushes it to target"""
@@ -35,16 +45,17 @@ def generate_tmp_file():
         print('*** preparing temporary file with name ' + tmp_file.name)
     adb.push(tmp_file.name, DEST_FOLDER_TARGET)
 
-    #gets full path on target for tmp_file
+    # gets full path on target for tmp_file
     global tmp_file_on_target
     tmp_file_on_target = (DEST_FOLDER_TARGET + os.path.basename(tmp_file.name))
     print('*** getting path to tmp_file ' + str(tmp_file.name))
 
-    #gets path to dest_folder_host
+    # gets path to dest_folder_host
     global tmp_file
     global dest_folder_host
     dest_folder_host = os.path.dirname(tmp_file.name)
     print('*** getting path to dest_folder_host ' + dest_folder_host)
+
 
 def is_emulator():
     result = adb.getserialno()
@@ -53,12 +64,40 @@ def is_emulator():
     else:
         return False
 
+
 def is_device_available():
     result = adb.getserialno()
     if re.search('unknown', result[1]):
         return False
     else:
         return True
+
+
+def install_apk():
+    adb.install(PATH_TO_VALID_APK)
+
+
+class TestGeneral(unittest.TestCase):
+    def test_get_opts_string(self):
+        opts = ["opt1"]
+        result = adb._convert_opts(opts)
+        expect(result).should.be.a('str')
+
+    def test_get_opts_single_opt(self):
+        opts = ["opt1"]
+        result = adb._convert_opts(opts)
+        expect(result).should.match(r'^opt1$')
+
+    def test_get_opts_multiple_opt(self):
+        opts = ["-r", "-b", "-a"]
+        result = adb._convert_opts(opts)
+        expect(result).should.match(r'^-r -b -a$')
+
+    def test_get_opts_empty_opt(self):
+        opts = []
+        result = adb._convert_opts(opts)
+        expect(result).should.match(r'^$')
+
 
 class TestPush(unittest.TestCase):
     def test_push(self):
@@ -71,6 +110,7 @@ class TestPush(unittest.TestCase):
         global tmp_file
         result = adb.push(NON_EXISTING_DIR, DEST_FOLDER_TARGET)
         self.assertNotEqual(str(result), 0)
+
 
 class TestPull(unittest.TestCase):
     def test_pull(self):
@@ -85,47 +125,50 @@ class TestPull(unittest.TestCase):
         result = adb.pull(tmp_file_on_target, NON_EXISTING_DIR)
         self.assertNotEqual(str(result), 0)
 
+
 class TestDevices(unittest.TestCase):
     def test_devices(self):
         result = adb.devices()
-        #don't check output code in result but presence of "device" string
+        # don't check output code in result but presence of "device" string
         self.assertRegexpMatches(result[1], '\\tdevice')
 
     def test_devices_l_option(self):
         result = adb.devices('-l')
         self.assertRegexpMatches(result[1], 'model:')
 
+
 class TestShell(unittest.TestCase):
     def test_shell(self):
         result = adb.shell('ls')
-        #search for folders which will be for sure on Adnroid OS
+        # search for folders which will be for sure on Adnroid OS
         self.assertRegexpMatches(result[1], '(\\r\\nroot|\\r\\nsys|\\r\\nsystem)')
 
     def test_shell_w_option(self):
         result = adb.shell('ls -l')
-        #search for time attribute which is for sure present in "-l" option
+        # search for time attribute which is for sure present in "-l" option
         self.assertRegexpMatches(result[1], '[0-2][0-9]:[0-5][0-9]')
 
     def test_shell_misspelled(self):
         result = adb.shell('misspelled')
         self.assertRegexpMatches(result[1], 'not found')
 
+
 class TestExec(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         """Prepares full adb commands for tests"""
-        #assembles "adb push" command
+        # assembles "adb push" command
         global tmp_file
         global adb_push
-        adb_push = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PUSH, tmp_file.name, \
-        DEST_FOLDER_TARGET ]
+        adb_push = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PUSH, tmp_file.name, \
+                    DEST_FOLDER_TARGET]
 
-        #assembles "adb pull" command
+        # assembles "adb pull" command
         global tmp_file_on_target
         global dest_folder_host
         global adb_pull
-        adb_pull = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, tmp_file_on_target, \
-        dest_folder_host ]
+        adb_pull = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, tmp_file_on_target, \
+                    dest_folder_host]
 
     def test_exec_adb_push(self):
         global adb_push
@@ -140,24 +183,25 @@ class TestExec(unittest.TestCase):
         self.assertEqual(result, POSITIVE_EXP_RESULT_WO_OUTPUT)
 
     def test_exec_incomplete_argument(self):
-        #4th argument is missing in adb_command
+        # 4th argument is missing in adb_command
         global POSITIVE_EXP_RESULT_WO_OUTPUT
-        adb_command = [ v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, tmp_file_on_target]
+        adb_command = [v.ADB_COMMAND_PREFIX, v.ADB_COMMAND_PULL, tmp_file_on_target]
         result = adb.exec_command(adb_command)
         self.assertEqual(result, POSITIVE_EXP_RESULT_WO_OUTPUT)
 
     def test_exec_missing_argument(self):
-        #no argument at all
+        # no argument at all
         adb_command = None
         result = adb.exec_command(adb_command)
         self.assertNotEqual(str(result), 0)
+
 
 class TestInstall(unittest.TestCase):
     @classmethod
     def setUp(self):
         """Deletes *.apk if it already installed"""
         global VALID_PACKAGE_NAME
-        result = adb.shell('pm list packages | grep ' +  VALID_PACKAGE_NAME)
+        result = adb.shell('pm list packages | grep ' + VALID_PACKAGE_NAME)
         if re.search(exp_install_cmd_output, result[1]):
             print('*** uninstalling existing ' + VALID_PACKAGE_NAME)
             adb.uninstall(VALID_PACKAGE_NAME)
@@ -166,35 +210,35 @@ class TestInstall(unittest.TestCase):
             it is not yet installed')
 
     @unittest.skipIf(is_emulator(), 'skip if run on emulator')
-    def test_install(self):
-        global PATH_TO_VALID_APK
-        result = adb.install(PATH_TO_VALID_APK)
-        self.assertRegexpMatches(result[1], 'Success')
+    def test_install_wo_opts(self):
+        opts = []
+        result = adb.install(PATH_TO_VALID_APK, opts)
+        result[1].should.match(r'Success')
 
     @unittest.skipIf(is_emulator(), 'skip if run on emulator')
-    def test_install_reinstall(self):
-        global PATH_TO_VALID_APK
-        self.test_install_p()
-        result = adb.install(PATH_TO_VALID_APK, '-r')
-        self.assertRegexpMatches(result[1], 'Success')
+    def test_install_w_opt(self):
+        opts = ["-t"]
+        result = adb.install(PATH_TO_VALID_APK, opts)
+        result[1].should.match(r'Success')
 
     @unittest.skipIf(is_emulator(), 'skip if run on emulator')
-    def test_install_sdcard(self):
-        global PATH_TO_VALID_APK
-        result = adb.install(PATH_TO_VALID_APK, '-s')
-        self.assertRegexpMatches(result[1], 'Success')
+    def test_install_multiple_opts(self):
+        opts = ["-t", "-s"]
+        result = adb.install(PATH_TO_VALID_APK, opts)
+        result[1].should.match(r'Success')
 
     @unittest.skipIf(is_emulator(), 'skip if run on emulator')
-    def test_install_all_opts(self):
-        global PATH_TO_VALID_APK
-        result = adb.install(PATH_TO_VALID_APK, '-r', '-s', '-l', '-d', '-t')
-        self.assertRegexpMatches(result[1], 'Success')
+    def test_install_wrong_opts(self):
+        opts = ["-wrong_opt"]
+        result = adb.install(PATH_TO_VALID_APK, opts)
+        result[1].should.match(r'Error')
 
     @unittest.skipIf(is_emulator(), 'skip if run on emulator')
     def test_install_invalid_apk(self):
-        global PATH_TO_INVALID_APK
-        result = adb.install(PATH_TO_INVALID_APK)
+        opts = []
+        result = adb.install(PATH_TO_INVALID_APK, opts)
         self.assertRegexpMatches(result[1], 'INSTALL_FAILED_INVALID_APK')
+
 
 class TestUninstall(unittest.TestCase):
     @classmethod
@@ -203,7 +247,7 @@ class TestUninstall(unittest.TestCase):
         global VALID_PACKAGE_NAME
         global PATH_TO_VALID_APK
 
-        result = adb.shell('pm list packages | grep ' +  VALID_PACKAGE_NAME)
+        result = adb.shell('pm list packages | grep ' + VALID_PACKAGE_NAME)
 
         if not re.search(exp_install_cmd_output, result[1]):
             print('*** installing ' + VALID_PACKAGE_NAME)
@@ -230,34 +274,40 @@ class TestUninstall(unittest.TestCase):
         result = adb.uninstall(INVALID_PACKAGE_NAME)
         self.assertRegexpMatches(result[1], 'DELETE_FAILED_INTERNAL_ERROR')
 
+
 class TestGetSerialNumber(unittest.TestCase):
     def test_getserialno(self):
         result = adb.getserialno()
         self.assertNotRegexpMatches(result[1], 'unknown')
 
+
 class TestWaitForDevice(unittest.TestCase):
-    #device should be available, otherwise test runs forever
+    # device should be available, otherwise test runs forever
     @unittest.skipUnless(is_device_available(), 'device not available')
     def test_wait_for_device(self):
         global POSITIVE_EXP_RESULT_WO_OUTPUT
         result = adb.wait_for_device()
         self.assertEqual(result, POSITIVE_EXP_RESULT_WO_OUTPUT)
 
+
 class TestKillServer(unittest.TestCase):
     def test_kill_server(self):
         result = adb.kill_server()
         self.assertEqual(result[0], POSITIVE_EXP_RESULT)
-        adb.start_server()#start server immediately in order to avoid fail of next test
+        adb.start_server()  # start server immediately in order to avoid fail of next test
+
 
 class TestStartServer(unittest.TestCase):
     def test_start_server(self):
         result = adb.start_server()
         self.assertEqual(result[0], POSITIVE_EXP_RESULT)
 
+
 class TestGetState(unittest.TestCase):
     def test_get_state(self):
         result = adb.get_state()
         self.assertEqual(result[1], 'device\n')
+
 
 if __name__ == '__main__':
     unittest.main()
